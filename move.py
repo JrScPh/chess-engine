12d0434e840788d39bc0c70bf0a31b7d49963b39
Jared Scott Phillips <jaredscottphillips13@gmail.com>
2026-05-26 15:18:52 -0500

    Create move.py

---
+++ move.py
@@ -0,0 +1,24 @@
+from constants import EMPTY, QUIET, PROMO_Q, PROMO_R, PROMO_B, PROMO_N, CASTLE_KING, CASTLE_QUEEN, EN_PASSANT
+
+class Move:
+    def __init__(self, from_sq, to_sq, flag=QUIET, captured=EMPTY):
+        self.from_sq = from_sq
+        self.to_sq = to_sq
+        self.flag = flag
+        self.captured = captured
+
+    @property
+    def is_capture(self):
+        return self.captured != EMPTY
+
+    @property
+    def is_promotion(self):
+        return self.flag in (PROMO_Q, PROMO_R, PROMO_B, PROMO_N)
+
+    @property
+    def is_castle(self):
+        return self.flag in (CASTLE_KING, CASTLE_QUEEN)
+
+    @property
+    def is_en_passant(self):
+        return self.flag == EN_PASSANT
